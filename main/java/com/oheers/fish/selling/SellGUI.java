5d337152415175f25996ffc7f564a778a8f0de32
Oheers <38732412+Oheers@users.noreply.github.com>
2021-04-17 13:23:36 +0100

    Region-whitelist to give custom fish to players. (WorldGuard) (#9)
    Co-authored-by: Ed<38732412+Eejayinq@users.noreply.github.com>

--- main/java/com/oheers/fish/selling/SellGUI.java
+++ main/java/com/oheers/fish/selling/SellGUI.java
@@ -78,19 +78,6 @@ public class SellGUI {
         menu.setItem(35, fill);
     }
 
-    public void errorFiller() {
-        // the gray glass panes at the bottom
-        ItemStack fill = new ItemStack(Material.GRAY_STAINED_GLASS_PANE);
-        ItemMeta fillMeta = fill.getItemMeta();
-        fillMeta.setDisplayName(ChatColor.RESET + "");
-        fill.setItemMeta(fillMeta);
-
-        // sets it as a default menu item that won't be dropped in a .close() request
-        this.errorFiller = WorthNBT.attributeDefault(fill);
-
-        addFiller(this.filler);
-    }
-
     public void setSellItem() {
         ItemStack sIcon = new ItemStack(Material.GOLD_INGOT);
         ItemMeta sellMeta = sIcon.getItemMeta();
