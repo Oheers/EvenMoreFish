e7f36d156d9b845588d1e7eb686923585617157c
Ed Ratner <38732412+Eejayinq@users.noreply.github.com>
2021-04-22 18:16:00 +0100

    Taking well-deserved priority over other plugins using the playerfishevent

--- main/java/com/oheers/fish/fishing/FishEvent.java
+++ main/java/com/oheers/fish/fishing/FishEvent.java
@@ -13,6 +13,7 @@ import org.bukkit.block.Biome;
 import org.bukkit.entity.Item;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerFishEvent;
 
@@ -46,7 +47,7 @@ public class FishEvent implements Listener, Runnable {
             "FLINT_AND_STEEL"
     );
 
-    @EventHandler
+    @EventHandler (priority = EventPriority.HIGHEST)
     public void onFish(PlayerFishEvent event) {
 
         if (EvenMoreFish.mainConfig.getEnabled()) {
