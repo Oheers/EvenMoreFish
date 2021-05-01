8884d831a51363e4958cee2e89312a5789159a7f
Ed Ratner <38732412+Eejayinq@users.noreply.github.com>
2021-05-01 20:17:30 +0100

    Initial adding of feature + debug messages

--- main/java/com/oheers/fish/competition/AutoRunner.java
+++ main/java/com/oheers/fish/competition/AutoRunner.java
@@ -1,20 +1,35 @@
 package com.oheers.fish.competition;
 
 import com.oheers.fish.EvenMoreFish;
-import com.oheers.fish.config.MainConfig;
 import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.scheduler.BukkitTask;
 
 import java.time.LocalTime;
+import java.util.Calendar;
 
 public class AutoRunner {
 
     static String timeKey;
 
+    private static BukkitTask task;
+
     public static void init() {
-        new BukkitRunnable(){
+        task = new BukkitRunnable(){
             public void run(){
                 // creates a key similar to the time key given in config.yml
                 timeKey = String.format("%02d", LocalTime.now().getHour()) + ":" + String.format("%02d", LocalTime.now().getMinute());
+                // if we're using a day criteria
+                if (EvenMoreFish.mainConfig.isDaySpecific()) {
+                    System.out.println("using");
+                    // loads up a rescue to occur at the new day if the day isn't registered
+                    if (!EvenMoreFish.mainConfig.getActiveDays().contains(Calendar.getInstance().get(Calendar.DAY_OF_WEEK))) {
+                        task.cancel();
+                        System.out.println("rescuing in: " + Integer.toString(86401 - LocalTime.now().toSecondOfDay()) + " seconds.");
+                        rescue(86401 - LocalTime.now().toSecondOfDay());
+                        return;
+                    }
+                }
+
                 // if that time key fits into the metaphoric lock that is the arrayList "competitionTimes"
                 if (EvenMoreFish.mainConfig.getCompetitionTimes().contains(timeKey)) {
                     // if there isn't a competition going on
@@ -26,5 +41,15 @@ public class AutoRunner {
             }
             // delay is set to start at bang on :00 of every minute, period is set to run once a minute
         }.runTaskTimer(EvenMoreFish.getProvidingPlugin(EvenMoreFish.class), (60-LocalTime.now().getSecond())*20, 20*60);
+    }
+
+    // "rescues" the init() method after a new day begins
+    private static void rescue(int ticksLeft) {
+        new BukkitRunnable() {
+            public void run() {
+                init();
+            }
+        }.runTaskLater(EvenMoreFish.getProvidingPlugin(EvenMoreFish.class), ticksLeft);
+        init();
     }
 }
