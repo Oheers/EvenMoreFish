bfd541b588a3a7ecd7e6b41448a4acdced572af3
Oheers <38732412+Oheers@users.noreply.github.com>
2021-12-04 10:48:05 +0000

    interact-event now only fires when right clicking air.

--- main/java/com/oheers/fish/events/FishInteractEvent.java
+++ main/java/com/oheers/fish/events/FishInteractEvent.java
@@ -5,6 +5,7 @@ import com.oheers.fish.competition.reward.Reward;
 import com.oheers.fish.fishing.items.Fish;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.EquipmentSlot;
 import org.bukkit.inventory.ItemStack;
@@ -24,7 +25,8 @@ public class FishInteractEvent implements Listener {
     @EventHandler
     public void interactEvent(PlayerInteractEvent event) {
         // If there is no item, or the player is sneaking (to place the head), the item isn't actually a fish, or the event fired for the off hand don't do anything
-        if (event.getItem() == null || event.getPlayer().isSneaking() || !FishUtils.isFish(event.getItem()) || event.getHand() == EquipmentSlot.OFF_HAND) {
+        if (event.getItem() == null || event.getPlayer().isSneaking() || !FishUtils.isFish(event.getItem()) || event.getHand() == EquipmentSlot.OFF_HAND
+         || event.getAction().equals(Action.RIGHT_CLICK_AIR)) {
             return;
         }
         // Creates a replica of the fish we can use
