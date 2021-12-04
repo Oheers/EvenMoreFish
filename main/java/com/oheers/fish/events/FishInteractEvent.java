a690ad627531926d3dad6f530625ec3eee70f0a2
Oheers <38732412+Oheers@users.noreply.github.com>
2021-12-04 10:49:02 +0000

    Fixed a bug with the last commit.

--- main/java/com/oheers/fish/events/FishInteractEvent.java
+++ main/java/com/oheers/fish/events/FishInteractEvent.java
@@ -26,7 +26,7 @@ public class FishInteractEvent implements Listener {
     public void interactEvent(PlayerInteractEvent event) {
         // If there is no item, or the player is sneaking (to place the head), the item isn't actually a fish, or the event fired for the off hand don't do anything
         if (event.getItem() == null || event.getPlayer().isSneaking() || !FishUtils.isFish(event.getItem()) || event.getHand() == EquipmentSlot.OFF_HAND
-         || event.getAction().equals(Action.RIGHT_CLICK_AIR)) {
+         || !event.getAction().equals(Action.RIGHT_CLICK_AIR)) {
             return;
         }
         // Creates a replica of the fish we can use
