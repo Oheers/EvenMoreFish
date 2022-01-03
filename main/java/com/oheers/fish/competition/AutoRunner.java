48a67ec602ae46e300d1e997212223423103a8da
Oheers <38732412+Oheers@users.noreply.github.com>
2022-01-03 10:17:19 +0000

    Fixed the competition auto-runner failing on low-tps servers.

--- main/java/com/oheers/fish/competition/AutoRunner.java
+++ main/java/com/oheers/fish/competition/AutoRunner.java
@@ -10,26 +10,46 @@ public class AutoRunner {
 
     static String timeKey;
 
+    static int lastMinute;
+
     public static void init() {
         new BukkitRunnable(){
             public void run(){
-                // creates a key similar to the time key given in config.yml
-                timeKey = String.format("%02d", LocalTime.now().getHour()) + ":" + String.format("%02d", LocalTime.now().getMinute());
+                // If the minute hasn't been checked against the competition queue.
+                if (!wasMinuteChecked()) {
+                    // creates a key similar to the time key given in config.yml
+                    timeKey = String.format("%02d", LocalTime.now().getHour()) + ":" + String.format("%02d", LocalTime.now().getMinute());
 
-                // Obtaining how many minutes have passed since midnight last Sunday
-                String day = LocalDate.now().getDayOfWeek().toString();
-                int weekMinute = EvenMoreFish.competitionQueue.generateTimeCode(day, timeKey);
+                    // Obtaining how many minutes have passed since midnight last Sunday
+                    String day = LocalDate.now().getDayOfWeek().toString();
+                    int weekMinute = EvenMoreFish.competitionQueue.generateTimeCode(day, timeKey);
 
-                // Beginning the competition set for schedule
-                if (EvenMoreFish.competitionQueue.competitions.containsKey(weekMinute)) {
-                    if (!Competition.isActive()) {
-                        EvenMoreFish.active = EvenMoreFish.competitionQueue.competitions.get(weekMinute);
-                        EvenMoreFish.active.begin(false);
+                    // Beginning the competition set for schedule
+                    if (EvenMoreFish.competitionQueue.competitions.containsKey(weekMinute)) {
+                        if (!Competition.isActive()) {
+                            EvenMoreFish.active = EvenMoreFish.competitionQueue.competitions.get(weekMinute);
+                            EvenMoreFish.active.begin(false);
+                        }
                     }
                 }
             }
 
-            // delay is set to start at bang on :00 of every minute, period is set to run once a minute
-        }.runTaskTimer(EvenMoreFish.getProvidingPlugin(EvenMoreFish.class), (60-LocalTime.now().getSecond())*20, 20*60);// (60-LocalTime.now().getSecond())*20
+            // delay is set to start at bang on :00 of every minute (assuming the tps to be 20), period is set to run once a minute
+        }.runTaskTimer(EvenMoreFish.getProvidingPlugin(EvenMoreFish.class), (60-LocalTime.now().getSecond())*20, 20);
+    }
+
+    /**
+     * Uses the last minute to work out whether the plugin should run calculations for this minute or not, it also
+     * automatically sets the lastMinute to the current minute if returning true.
+     *
+     * @return Whether minute checks need running to determine whether a competition needs to start.
+     */
+    private static boolean wasMinuteChecked() {
+        if (lastMinute != LocalTime.now().getMinute()) {
+            lastMinute = LocalTime.now().getMinute();
+            return false;
+        }
+
+        return true;
     }
 }
