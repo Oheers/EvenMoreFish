f9468687c010d032e84aa19b9bc985c3ad3c33e7
Omer Oreg <sarhatabaot1@gmail.com>
2022-06-27 13:17:22 +0300

    Added getString getFloat to NbtUtils

--- src/main/java/com/oheers/fish/NbtUtils.java
+++ src/main/java/com/oheers/fish/NbtUtils.java
@@ -6,6 +6,7 @@ import org.bukkit.NamespacedKey;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 public class NbtUtils {
     /*
@@ -18,28 +19,74 @@ public class NbtUtils {
             compound for evenmorefish:
                             key: value
      */
-    public static boolean hasKey(final @NotNull NBTItem nbtItem, final String key) {
+    public static boolean hasKey(final @NotNull NBTCompound nbtCompound, final String key) {
         NamespacedKey namespacedKey = getNamespacedKey(key);
 
         //Pre NBT-API PR
-        if(Boolean.TRUE.equals(nbtItem.hasKey("PublicBukkitValues"))) {
-            NBTCompound publicBukkitValues = nbtItem.getCompound("PublicBukkitValues");
+        if(Boolean.TRUE.equals(nbtCompound.hasKey("PublicBukkitValues"))) {
+            NBTCompound publicBukkitValues = nbtCompound.getCompound("PublicBukkitValues");
             if(Boolean.TRUE.equals(publicBukkitValues.hasKey(namespacedKey.toString())))
                 return true;
         }
 
         //NBT API PR
-        if(Boolean.TRUE.equals(nbtItem.hasKey(namespacedKey.toString())))
+        if(Boolean.TRUE.equals(nbtCompound.hasKey(namespacedKey.toString())))
             return true;
 
         //NBT COMPAT
-        if(Boolean.TRUE.equals(nbtItem.hasKey(namespacedKey.getNamespace()))) {
-            NBTCompound emfCompound = nbtItem.getCompound(namespacedKey.getNamespace());
+        if(Boolean.TRUE.equals(nbtCompound.hasKey(namespacedKey.getNamespace()))) {
+            NBTCompound emfCompound = nbtCompound.getCompound(namespacedKey.getNamespace());
             return Boolean.TRUE.equals(emfCompound.hasKey(namespacedKey.getKey()));
         }
 
         return false;
     }
+
+
+    public static @Nullable String getString(final @NotNull NBTCompound nbtCompound, final String key) {
+        NamespacedKey namespacedKey = getNamespacedKey(key);
+        if(Boolean.TRUE.equals(nbtCompound.hasKey("PublicBukkitValues"))) {
+            NBTCompound publicBukkitValues = nbtCompound.getCompound("PublicBukkitValues");
+            if(Boolean.TRUE.equals(publicBukkitValues.hasKey(namespacedKey.toString())))
+                return publicBukkitValues.getString(namespacedKey.toString());
+        }
+
+        //NBT API PR
+        if(Boolean.TRUE.equals(nbtCompound.hasKey(namespacedKey.toString())))
+            return nbtCompound.getString(namespacedKey.toString());
+
+        //NBT COMPAT
+        if(Boolean.TRUE.equals(nbtCompound.hasKey(namespacedKey.getNamespace()))) {
+            NBTCompound emfCompound = nbtCompound.getCompound(namespacedKey.getNamespace());
+            if(Boolean.TRUE.equals(emfCompound.hasKey(namespacedKey.getKey())))
+                return nbtCompound.getString(namespacedKey.getKey());
+        }
+
+        return null;
+    }
+
+    public static @Nullable Float getFloat(final @NotNull NBTCompound nbtCompound, final String key) {
+        NamespacedKey namespacedKey = getNamespacedKey(key);
+        if(Boolean.TRUE.equals(nbtCompound.hasKey("PublicBukkitValues"))) {
+            NBTCompound publicBukkitValues = nbtCompound.getCompound("PublicBukkitValues");
+            if(Boolean.TRUE.equals(publicBukkitValues.hasKey(namespacedKey.toString())))
+                return publicBukkitValues.getFloat(namespacedKey.toString());
+        }
+
+        //NBT API PR
+        if(Boolean.TRUE.equals(nbtCompound.hasKey(namespacedKey.toString())))
+            return nbtCompound.getFloat(namespacedKey.toString());
+
+        //NBT COMPAT
+        if(Boolean.TRUE.equals(nbtCompound.hasKey(namespacedKey.getNamespace()))) {
+            NBTCompound emfCompound = nbtCompound.getCompound(namespacedKey.getNamespace());
+            if(Boolean.TRUE.equals(emfCompound.hasKey(namespacedKey.getKey())))
+                return nbtCompound.getFloat(namespacedKey.getKey());
+        }
+
+        return null;
+    }
+
 
     @Contract("_ -> new")
     private static @NotNull NamespacedKey getNamespacedKey(final String key) {
