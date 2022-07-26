9319412a08d06febfeafb68bf2fc6d971c5aee95
evlad <emmanuelvlad@gmail.com>
2022-07-26 22:35:55 +0200

    feat: getFish api method from rarity and name

--- src/main/java/com/oheers/fish/api/EMFAPI.java
+++ src/main/java/com/oheers/fish/api/EMFAPI.java
@@ -1,5 +1,9 @@
 package com.oheers.fish.api;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.Map.Entry;
+
 import org.bukkit.block.Skull;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
@@ -8,6 +12,7 @@ import com.oheers.fish.EvenMoreFish;
 import com.oheers.fish.FishUtils;
 import com.oheers.fish.exceptions.InvalidFishException;
 import com.oheers.fish.fishing.items.Fish;
+import com.oheers.fish.fishing.items.Rarity;
 
 public class EMFAPI {
 	private final EvenMoreFish plugin;
@@ -34,5 +39,17 @@ public class EMFAPI {
 
 	public Fish getFish(Skull skull, Player fisher) throws InvalidFishException {
 		return FishUtils.getFish(skull, fisher);
+	}
+
+	public Optional<Fish> getFish(String rarity, String name) {
+		for (Entry<Rarity, List<Fish>> e : EvenMoreFish.fishCollection.entrySet()) {
+			if (e.getKey().getValue().equalsIgnoreCase(rarity)) {
+				Optional<Fish> optFish = e.getValue().stream().filter(f -> f.getName().equalsIgnoreCase(name)).findFirst();
+				if (optFish.isPresent()) {
+					return optFish;
+				}
+			}
+		}
+		return Optional.empty();
 	}
 }
