9b8efa2f1cbfc9bfa053589e4ec2ba4febd435b5
Omer Oreg <omeroreg@gmail.com>
2023-07-06 23:40:17 +0300

    Ensure everyone uses the same jvm vendor

--- build.gradle.kts
+++ build.gradle.kts
@@ -111,5 +111,7 @@ tasks {
 java {
     toolchain {
         languageVersion.set(JavaLanguageVersion.of(8))
+        vendor.set(JvmVendorSpec.ADOPTIUM)
     }
 }
+
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -35,3 +35,7 @@ dependencyResolutionManagement {
         }
     }
 }
+
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.5.0"
+}
