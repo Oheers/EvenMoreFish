46e0625b21d52565525557e55ac3266712fc360d
Omer Oreg <sarhatabaot1@gmail.com>
2023-12-31 11:57:04 +0200

    Update foojay plugin
    Really to allow us to create a draft pr. But update was needed anyways.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -56,5 +56,5 @@ dependencyResolutionManagement {
 }
 
 plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version "0.5.0"
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.7.0"
 }
