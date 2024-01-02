02f06378f497bd778fc713987bf02e8bf9bc7329
Omer Oreg <sarhatabaot1@gmail.com>
2024-01-02 18:11:39 +0200

    Update FishingGUI.java

--- even-more-fish-plugin/src/main/java/com/oheers/fish/gui/FishingGUI.java
+++ even-more-fish-plugin/src/main/java/com/oheers/fish/gui/FishingGUI.java
@@ -12,7 +12,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
-//todo, not being used??
 public class FishingGUI extends Gui {
 
     List<Button> guiButtons = new ArrayList<>();
