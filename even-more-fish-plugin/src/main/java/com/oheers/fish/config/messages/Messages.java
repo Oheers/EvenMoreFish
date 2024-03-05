5bf609d92ee6971aadad8464d90fe77910d58ca8
FireML <106587317+FireML@users.noreply.github.com>
2024-03-05 12:32:00 +0000

    Fix Message Config
    Adds an override to the loadFile method to get the correct plugin resource

--- even-more-fish-plugin/src/main/java/com/oheers/fish/config/messages/Messages.java
+++ even-more-fish-plugin/src/main/java/com/oheers/fish/config/messages/Messages.java
@@ -1,11 +1,20 @@
 package com.oheers.fish.config.messages;
 
 import com.oheers.fish.config.ConfigBase;
+import com.oheers.fish.config.MainConfig;
 import org.bukkit.configuration.file.FileConfiguration;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
+import java.util.logging.Level;
 
 public class Messages extends ConfigBase {
 
     private static Messages instance = null;
+    private String localeFileName = null;
     public FileConfiguration config;
 
     public Messages() {
@@ -15,6 +24,35 @@ public class Messages extends ConfigBase {
 
     public static Messages getInstance() {
         return instance;
+    }
+
+    @Override
+    public File loadFile(File directory) {
+        if (!directory.exists()) {
+            directory.mkdirs();
+        }
+        File configFile = new File(directory, getFileName());
+        if (!configFile.exists()) {
+            try {
+                configFile.createNewFile();
+            } catch (IOException ex) {
+                getPlugin().getLogger().log(Level.SEVERE, ex.getMessage(), ex);
+            }
+            if (localeFileName == null) {
+                localeFileName = "messages_" + MainConfig.getInstance().getLocale() + ".yml";
+            }
+            InputStream stream = getPlugin().getResource("locales/" + localeFileName);
+            if (stream == null) {
+                getPlugin().getLogger().log(Level.SEVERE, "Could not retrieve " + localeFileName);
+                return null;
+            }
+            try {
+                Files.copy(stream, configFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
+            } catch (IOException ex) {
+                getPlugin().getLogger().log(Level.SEVERE, ex.getMessage(), ex);
+            }
+        }
+        return configFile;
     }
 
     public String getSTDPrefix() {
