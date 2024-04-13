c607acae3ed45c79ad7483329086160bd8094ffc
Omer Oreg <omeroreg@gmail.com>
2024-04-13 10:38:46 +0300

    Impl help, top, shop commands

--- even-more-fish-plugin/src/main/java/com/oheers/fish/commands/CommandUtil.java
+++ even-more-fish-plugin/src/main/java/com/oheers/fish/commands/CommandUtil.java
@@ -1,6 +1,50 @@
 package com.oheers.fish.commands;
 
 
+import com.oheers.fish.EvenMoreFish;
+import com.oheers.fish.FishUtils;
+import com.oheers.fish.config.messages.ConfigMessage;
+import com.oheers.fish.config.messages.Message;
+import com.oheers.fish.config.messages.Messages;
+import com.oheers.fish.permissions.AdminPerms;
+import com.oheers.fish.permissions.UserPerms;
+import org.bukkit.command.CommandSender;
+
+import java.util.Arrays;
+import java.util.List;
+
 public class CommandUtil {
     public static final String MAIN_COMMAND = "emf|evenmorefish";
+    public static final String ADMIN_COMMAND = "emf|evenmorefish admin";
+    public static String formGeneralHelp(CommandSender user) {
+
+        //return new Message(ConfigMessage.HELP_GENERAL).getRawMessage(true, false);
+
+        StringBuilder out = new StringBuilder();
+        List<String> commands = Arrays.asList(new Message(ConfigMessage.HELP_GENERAL).getRawMessage(true, false).split("\n"));
+
+        String escape = "\n";
+        if (EvenMoreFish.getInstance().getPermission() != null && user != null) {
+            for (int i = 0; i < commands.size(); i++) {
+                if (i == commands.size() - 1) escape = "";
+                if (commands.get(i).contains("/emf admin")) {
+                    if (EvenMoreFish.getInstance().getPermission().has(user, AdminPerms.ADMIN)) out.append(commands.get(i)).append(escape);
+                } else if (commands.get(i).contains("/emf top")) {
+                    if (EvenMoreFish.getInstance().getPermission().has(user, UserPerms.TOP)) out.append(commands.get(i)).append(escape);
+                } else if (commands.get(i).contains("/emf shop")) {
+                    if (EvenMoreFish.getInstance().getPermission().has(user, UserPerms.SHOP)) out.append(commands.get(i)).append(escape);
+                } else if (commands.get(i).contains("/emf toggle")) {
+                    if (EvenMoreFish.getInstance().getPermission().has(user, UserPerms.TOGGLE)) out.append(commands.get(i)).append(escape);
+                } else out.append(commands.get(i)).append(escape);
+            }
+        } else {
+            for (int i = 0; i < commands.size(); i++) {
+                if (i == commands.size() - 1) escape = "";
+                out.append(FishUtils.translateHexColorCodes(Messages.getInstance().getSTDPrefix() + commands.get(i) + escape));
+            }
+        }
+
+        return out.toString();
+
+    }
 }
--- even-more-fish-plugin/src/main/java/com/oheers/fish/commands/EMFCommand.java
+++ even-more-fish-plugin/src/main/java/com/oheers/fish/commands/EMFCommand.java
@@ -1,20 +1,23 @@
 package com.oheers.fish.commands;
 
 import co.aikar.commands.BaseCommand;
-import co.aikar.commands.annotation.CommandAlias;
-import co.aikar.commands.annotation.CommandPermission;
-import co.aikar.commands.annotation.Default;
-import co.aikar.commands.annotation.Subcommand;
+import co.aikar.commands.annotation.*;
 import co.aikar.commands.bukkit.contexts.OnlinePlayer;
+import com.earth2me.essentials.User;
 import com.oheers.fish.EvenMoreFish;
 import com.oheers.fish.competition.Competition;
+import com.oheers.fish.config.MainConfig;
 import com.oheers.fish.config.messages.ConfigMessage;
 import com.oheers.fish.config.messages.Message;
 import com.oheers.fish.config.messages.PrefixType;
 import com.oheers.fish.permissions.AdminPerms;
 import com.oheers.fish.permissions.UserPerms;
+import com.oheers.fish.selling.SellGUI;
+import org.bukkit.Bukkit;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.entity.Player;
+
 
 @CommandAlias(CommandUtil.MAIN_COMMAND)
 public class EMFCommand extends BaseCommand {
@@ -46,7 +49,50 @@ public class EMFCommand extends BaseCommand {
     }
 
     @Default
-    public void onHelp() {
-
+    @Subcommand("help")
+    public void onHelp(final CommandSender sender) {
+        sender.sendMessage(CommandUtil.formGeneralHelp(sender));
     }
+
+    @Subcommand("top")
+    @CommandPermission(UserPerms.TOP)
+    public void onTop(final CommandSender sender) {
+        if (!Competition.isActive()) {
+            new Message(ConfigMessage.NO_COMPETITION_RUNNING).broadcast(sender, true, true);
+            return;
+        }
+
+        if (sender instanceof Player) {
+            EvenMoreFish.getInstance().getActiveCompetition().sendPlayerLeaderboard((Player) sender);
+            return;
+        }
+
+        if (sender instanceof ConsoleCommandSender) {
+            EvenMoreFish.getInstance().getActiveCompetition().sendConsoleLeaderboard((ConsoleCommandSender) sender);
+        }
+    }
+
+    @Subcommand("shop")
+    @CommandPermission(UserPerms.SHOP)
+    public void onShop(final CommandSender sender, @Optional final OnlinePlayer onlinePlayer) {
+        if (MainConfig.getInstance().isEconomyDisabled()) {
+            new Message(ConfigMessage.ECONOMY_DISABLED).broadcast(sender, true, false);
+            return;
+        }
+
+
+        if (EvenMoreFish.getInstance().getPermission().has(sender, AdminPerms.ADMIN)) {
+            new SellGUI(onlinePlayer.player, true);
+            Message message = new Message(ConfigMessage.ADMIN_OPEN_FISH_SHOP);
+            message.setPlayer(onlinePlayer.player.getName());
+            message.broadcast(sender, true, true);
+            return;
+        }
+
+        if (sender instanceof Player) {
+            new SellGUI((Player) sender, true);
+        }
+    }
+
+
 }
