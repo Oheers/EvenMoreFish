6a68d5ab5c123aab65434f0e00bcb7be44f8fbbc
Omer Oreg <omeroreg@gmail.com>
2024-04-20 09:46:26 +0300

    Cleanup

--- even-more-fish-plugin/src/main/java/com/oheers/fish/competition/CompetitionType.java
+++ even-more-fish-plugin/src/main/java/com/oheers/fish/competition/CompetitionType.java
@@ -2,6 +2,10 @@ package com.oheers.fish.competition;
 
 public enum CompetitionType {
 
-    LARGEST_FISH, SPECIFIC_FISH, MOST_FISH, SPECIFIC_RARITY, LARGEST_TOTAL,
+    LARGEST_FISH,
+    SPECIFIC_FISH,
+    MOST_FISH,
+    SPECIFIC_RARITY,
+    LARGEST_TOTAL,
     RANDOM
 }
