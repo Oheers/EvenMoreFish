20faea801cb5ce5094a6e7307c4b577a79c4e639
FireML <106587317+FireML@users.noreply.github.com>
2024-05-08 22:57:36 +0100

    Add AuraSkillsXPRewardType
    Allows rewarding AuraSkills Skill XP

---
+++ even-more-fish-plugin/src/main/java/com/oheers/fish/competition/rewardtypes/external/AuraSkillsXPRewardType.java
@@ -0,0 +1,59 @@
+package com.oheers.fish.competition.rewardtypes.external;
+
+import com.oheers.fish.EvenMoreFish;
+import com.oheers.fish.api.reward.RewardType;
+import dev.aurelium.auraskills.api.AuraSkillsApi;
+import dev.aurelium.auraskills.api.registry.NamespacedId;
+import dev.aurelium.auraskills.api.skill.Skill;
+import dev.aurelium.auraskills.api.stat.Stat;
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.jetbrains.annotations.NotNull;
+
+public class AuraSkillsXPRewardType implements RewardType {
+
+    @Override
+    public void doReward(@NotNull Player player, @NotNull String key, @NotNull String value, Location hookLocation) {
+        // Pre-checks
+        String[] split = value.split(",");
+        if (split.length < 2) {
+            EvenMoreFish.getInstance().getLogger().warning("Invalid format for RewardType " + getIdentifier() + ": " + value);
+            EvenMoreFish.getInstance().getLogger().warning("Expected \"Name,Amount\"");
+            return;
+        }
+        String name = split[0];
+        double amount;
+        try {
+            amount = Double.parseDouble(split[1]);
+        } catch (NumberFormatException ex) {
+            EvenMoreFish.getInstance().getLogger().warning("Invalid number specified for RewardType " + getIdentifier() + ": " + split[2]);
+            return;
+        }
+
+        // Handle reward
+        AuraSkillsApi api = AuraSkillsApi.get();
+        Skill skill = api.getGlobalRegistry().getSkill(NamespacedId.fromString(name));
+        if (skill == null) {
+            EvenMoreFish.getInstance().getLogger().warning("Invalid stat specified for RewardType " + getIdentifier() + ": " + name);
+            return;
+        }
+        api.getUser(player.getUniqueId()).addSkillXp(skill, amount);
+    }
+
+    @Override
+    public @NotNull String getIdentifier() {
+        return "AURASKILLS_XP";
+    }
+
+    @Override
+    public @NotNull String getAuthor() {
+        return "FireML";
+    }
+
+    @Override
+    public @NotNull JavaPlugin getPlugin() {
+        return EvenMoreFish.getInstance();
+    }
+
+}
