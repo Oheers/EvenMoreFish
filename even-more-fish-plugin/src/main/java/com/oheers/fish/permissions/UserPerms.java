4946a20e05908dcba064bdaec72baa3448fb5ca5
Omer Oreg <sarhatabaot1@gmail.com>
2024-05-23 23:29:30 +0300

    Fixed improper ordering

--- even-more-fish-plugin/src/main/java/com/oheers/fish/permissions/UserPerms.java
+++ even-more-fish-plugin/src/main/java/com/oheers/fish/permissions/UserPerms.java
@@ -1,14 +1,14 @@
 package com.oheers.fish.permissions;
 
 public class UserPerms {
-    private UserPerms() {
-        throw new UnsupportedOperationException();
-    }
-
     public static final String USE_ROD = "emf.use_rod";
     public static final String SHOP = "emf.shop";
     public static final String TOGGLE = "emf.toggle";
     public static final String TOP = "emf.top";
     public static final String SELL_ALL = "emf.sellall";
     public static final String XMAS = "emf.xmas";
+
+    private UserPerms() {
+        throw new UnsupportedOperationException();
+    }
 }
