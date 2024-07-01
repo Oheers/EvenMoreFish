52cf12724011f3d86de96d475c1fdef60280ca51
FireML <firemlyt@gmail.com>
2024-07-01 16:46:41 +0100

    Add GUIUtils

---
+++ even-more-fish-plugin/src/main/java/com/oheers/fish/utils/GUIUtils.java
@@ -0,0 +1,83 @@
+package com.oheers.fish.utils;
+
+import com.oheers.fish.config.GUIConfig;
+import de.themoep.inventorygui.GuiPageElement;
+import org.bukkit.Material;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.inventory.ItemStack;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+
+public class GUIUtils {
+
+    public static ItemStack getExitItem() {
+        FileConfiguration config = GUIConfig.getInstance().getConfig();
+        return createItemStack(
+                config.getString("gui.global.exit.material", "structure_void"),
+                Material.STRUCTURE_VOID,
+                config.getString("gui.global.exit.name", "&cExit"),
+                config.getStringList("gui.global.exit.lore")
+        );
+    }
+
+    public static GuiPageElement getFirstPageButton() {
+        FileConfiguration config = GUIConfig.getInstance().getConfig();
+        return new GuiPageElement('f',
+                createItemStack(
+                        config.getString("gui.global.first-page.material", "arrow"),
+                        Material.ARROW,
+                        config.getString("gui.global.first-page.name", "&bFirst Page"),
+                        config.getStringList("gui.global.first-page.lore")
+                ),
+                GuiPageElement.PageAction.FIRST
+        );
+    }
+
+    public static GuiPageElement getNextPageButton() {
+        FileConfiguration config = GUIConfig.getInstance().getConfig();
+        return new GuiPageElement('n',
+                createItemStack(
+                        config.getString("gui.global.next-page.material", "paper"),
+                        Material.PAPER,
+                        config.getString("gui.global.next-page.name", "&bNext Page"),
+                        config.getStringList("gui.global.next-page.lore")
+                ),
+                GuiPageElement.PageAction.NEXT
+        );
+    }
+
+    public static GuiPageElement getPreviousPageButton() {
+        FileConfiguration config = GUIConfig.getInstance().getConfig();
+        return new GuiPageElement('p',
+                createItemStack(
+                        config.getString("gui.global.previous-page.material", "paper"),
+                        Material.PAPER,
+                        config.getString("gui.global.previous-page.name", "&bPrevious Page"),
+                        config.getStringList("gui.global.previous-page.lore")
+                ),
+                GuiPageElement.PageAction.PREVIOUS
+        );
+    }
+
+    public static GuiPageElement getLastPageButton() {
+        FileConfiguration config = GUIConfig.getInstance().getConfig();
+        return new GuiPageElement('l',
+                createItemStack(
+                        config.getString("gui.global.last-page.material", "arrow"),
+                        Material.ARROW,
+                        config.getString("gui.global.last-page.name", "&bLast Page"),
+                        config.getStringList("gui.global.last-page.lore")
+                ),
+                GuiPageElement.PageAction.LAST
+        );
+    }
+
+    public static ItemStack createItemStack(@NotNull String materialName, @NotNull Material defaultMaterial, @NotNull String display, @NotNull List<String> lore) {
+        return new ItemBuilder(materialName, defaultMaterial)
+                .withDisplay(display)
+                .withLore(lore)
+                .build();
+    }
+
+}
