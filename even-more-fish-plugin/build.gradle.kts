ee867bc58cc0635dab3ab3a922357f6cefdd98da
Omer Oreg <omeroreg@gmail.com>
2024-07-08 21:30:23 +0300

    Added a way to view build versions

--- even-more-fish-plugin/build.gradle.kts
+++ even-more-fish-plugin/build.gradle.kts
@@ -1,8 +1,15 @@
+import java.time.Instant
+import java.time.ZoneId
+import java.time.format.DateTimeFormatter
+import java.util.*
+
 plugins {
     `java-library`
     id("maven-publish")
     id("net.minecrell.plugin-yml.bukkit") version "0.6.0"
     id("com.github.johnrengelman.shadow") version "8.1.1"
+
+    alias(libs.plugins.grgit)
 }
 
 group = "com.oheers.evenmorefish"
@@ -198,6 +205,15 @@ tasks {
     }
 
     shadowJar {
+        manifest {
+            val buildNumber: String? by project
+
+            attributes["Specification-Title"] = "EvenMoreFish"
+            attributes["Specification-Version"] = project.version
+            attributes["Implementation-Title"] = grgit.branch.current().name
+            attributes["Implementation-Version"] = getBuildNumberOrDate()
+        }
+
         minimize()
 
         exclude("META-INF/**")
@@ -216,6 +232,8 @@ tasks {
     compileJava {
         options.compilerArgs.add("-parameters")
         options.isFork = true
+
+
     }
 }
 
@@ -224,5 +242,21 @@ java {
         languageVersion.set(JavaLanguageVersion.of(8))
         vendor.set(JvmVendorSpec.ADOPTIUM)
     }
+}
+
+fun getBuildNumberOrDate(): String? {
+    if ("".equals("master", ignoreCase = true)) {
+        val buildNumber: String? by project
+        if (buildNumber == null)
+            return "RELEASE"
+
+        return buildNumber
+    }
+
+    val time = DateTimeFormatter.ofPattern("yyyyMMdd-HHmm", Locale.ENGLISH)
+        .withZone(ZoneId.systemDefault())
+        .format(Instant.now())
+
+    return time
 }
 
