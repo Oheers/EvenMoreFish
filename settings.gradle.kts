1a481b069e133b2be318c806d7a8c4be3bc8105d
FireML <firemlyt@gmail.com>
2024-08-09 12:14:53 +0100

    Oraxen 1.171.0 -> 1.179.0

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -39,7 +39,7 @@ dependencyResolutionManagement {
             library("itemsadder-api", "com.github.LoneDev6:API-ItemsAdder:3.6.1")
             library("nbt-api", "de.tr7zw:item-nbt-api:2.13.1")
             library("denizen-api", "com.denizenscript:denizen:1.3.1-SNAPSHOT")
-            library("oraxen", "com.github.oraxen:oraxen:1.171.0")
+            library("oraxen", "com.github.oraxen:oraxen:1.179.0")
 
             library("ecoitems-api", "com.willfp:EcoItems:5.6.1")
             library("ecoitems-libreforge", "com.willfp:libreforge:4.21.1")
