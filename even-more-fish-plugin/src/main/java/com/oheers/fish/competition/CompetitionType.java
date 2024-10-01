8e532d6802651da4052e98564e17ed8204ac69e9
FireML <firemlyt@gmail.com>
2024-10-01 20:41:04 +0100

    Convert CompetitionType to an interface

--- even-more-fish-plugin/src/main/java/com/oheers/fish/competition/CompetitionType.java
+++ even-more-fish-plugin/src/main/java/com/oheers/fish/competition/CompetitionType.java
@@ -1,12 +1,25 @@
 package com.oheers.fish.competition;
 
-public enum CompetitionType {
-    LARGEST_FISH,
-    SPECIFIC_FISH,
-    MOST_FISH,
-    SPECIFIC_RARITY,
-    LARGEST_TOTAL,
-    RANDOM,
-    SHORTEST_FISH,
-    SHORTEST_TOTAL
+import com.oheers.fish.competition.leaderboard.LeaderboardHandler;
+import org.bukkit.plugin.Plugin;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.function.Consumer;
+
+public interface CompetitionType {
+
+    String getIdentifier();
+
+    String getAuthor();
+
+    Plugin getPlugin();
+
+    LeaderboardHandler getNewLeaderboard();
+
+    default void register() {
+        CompetitionManager.getInstance().registerType(this);
+    }
+
+    @Nullable Consumer<Competition> getTypeBeginLogic();
+
 }
