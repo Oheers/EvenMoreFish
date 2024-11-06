69af0c22299fbc88b75349b6db687881f2172ce9
David <david@conceptpc.ca>
2024-11-06 09:00:01 -0600

    Fix up: Add safety around null pointers

--- even-more-fish-plugin/src/main/java/com/oheers/fish/utils/nbt/NbtUtils.java
+++ even-more-fish-plugin/src/main/java/com/oheers/fish/utils/nbt/NbtUtils.java
@@ -59,7 +59,10 @@ public class NbtUtils {
             case COMPAT -> nbt.getCompound(namespacedKey.getNamespace()).getString(namespacedKey.getKey());
             case LEGACY -> {
                 if (nbt.hasTag(NbtKeys.PUBLIC_BUKKIT_VALUES)) {
-                    String baitString = nbt.getCompound(NbtKeys.PUBLIC_BUKKIT_VALUES).getString(namespacedKey.toString());
+                    ReadableNBT publicBukkitValues = nbt.getCompound(NbtKeys.PUBLIC_BUKKIT_VALUES);
+                    if (publicBukkitValues == null) yield null;
+                    String baitString = publicBukkitValues.getString(namespacedKey.toString());
+                    if (baitString == null) yield null;
                     yield baitString.isEmpty() ? null : baitString;
                 }
                 yield null;
