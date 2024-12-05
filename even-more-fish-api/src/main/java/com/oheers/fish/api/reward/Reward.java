020f8f7f8e917f124b3f8dade724f296f3496e32
FireML <fireml@firedev.uk>
2024-12-05 09:50:09 +0000

    Suppress deprecation in Reward class

--- even-more-fish-api/src/main/java/com/oheers/fish/api/reward/Reward.java
+++ even-more-fish-api/src/main/java/com/oheers/fish/api/reward/Reward.java
@@ -42,10 +42,11 @@ public class Reward {
 
     public @NotNull String getValue() { return this.value; }
 
+    // Ignore deprecation warnings, we need to keep the old event for outdated addons.
+    @SuppressWarnings("deprecation")
     public void rewardPlayer(@NotNull Player player, Location hookLocation) {
         if (getRewardType() == null) {
             EMFPlugin.getLogger().warning("No reward type found for key: " + getKey() + ". Falling back to the deprecated event-based rewards.");
-            // Ignore deprecation warnings, we need to keep this here for any outdated addons.
             EMFRewardEvent event = new EMFRewardEvent(this, player, fishVelocity, hookLocation);
             Bukkit.getPluginManager().callEvent(event);
             return;
