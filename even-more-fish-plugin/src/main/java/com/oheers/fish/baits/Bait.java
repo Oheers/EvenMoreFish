3fa4234b2eb8590c8511d443df58a45c5a22a6ae
FireML <fireml@firedev.uk>
2024-12-24 21:46:16 +0000

    Remove old constructor

--- even-more-fish-plugin/src/main/java/com/oheers/fish/baits/Bait.java
+++ even-more-fish-plugin/src/main/java/com/oheers/fish/baits/Bait.java
@@ -67,43 +67,6 @@ public class Bait {
     }
 
     /**
-     * This represents a bait, which can be used to boost the likelihood that a certain fish or fish rarity appears from
-     * the rod. All data is fetched from the config when the Bait object is created and then can be given out using
-     * the create() method. An error will be printed out into the console if the name of the bait cannot be found in the
-     * baits.yml file.
-     * <p>
-     * The plugin recognises the bait item from the create() method using NBT data, which can be applied using the
-     * BaitNBTManager class, which handles all the NBT thingies.
-     *
-     * @param name The name of the bait to be referenced from the baits.yml file
-     */
-    public Bait(String name) {
-        this.section = null;
-        this.name = name;
-
-        if (BaitFile.getInstance().getBaitTheme(name) != null) {
-            this.theme = FishUtils.translateColorCodes(BaitFile.getInstance().getBaitTheme(name));
-        } else {
-            this.theme = "&e";
-        }
-
-        setApplicationWeight(BaitFile.getInstance().getApplicationWeight(name));
-        setCatchWeight(BaitFile.getInstance().getCatchWeight(name));
-
-        this.boostRate = BaitFile.getInstance().getBoostRate();
-        this.maxApplications = BaitFile.getInstance().getMaxBaitApplication(this.name);
-        this.displayName = BaitFile.getInstance().getDisplayName(this.name);
-        this.dropQuantity = BaitFile.getInstance().getDropQuantity(this.name);
-
-        this.itemFactory = new ItemFactory("baits." + name, BaitFile.getInstance().getConfig());
-
-        this.itemFactory.enableDefaultChecks();
-        this.itemFactory.setItemDisplayNameCheck(true);
-
-        this.itemFactory.setDisplayName(FishUtils.translateColorCodes("&e" + name));
-    }
-
-    /**
      * This creates an item based on random settings in the yml files, adding things such as custom model data and glowing
      * effects.
      *
