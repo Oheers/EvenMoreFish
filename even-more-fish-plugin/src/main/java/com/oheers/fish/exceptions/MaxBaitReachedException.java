cbe30946d99970a7789bb5cda65bc54a79e31bfd
FireML <fireml@firedev.uk>
2025-01-07 21:49:34 +0000

    Annotate Exceptions

--- even-more-fish-plugin/src/main/java/com/oheers/fish/exceptions/MaxBaitReachedException.java
+++ even-more-fish-plugin/src/main/java/com/oheers/fish/exceptions/MaxBaitReachedException.java
@@ -2,12 +2,13 @@ package com.oheers.fish.exceptions;
 
 import com.oheers.fish.baits.ApplicationResult;
 import com.oheers.fish.baits.Bait;
+import org.jetbrains.annotations.NotNull;
 
 public class MaxBaitReachedException extends Exception {
 
-    ApplicationResult recoveryResult;
+    private final ApplicationResult recoveryResult;
 
-    public MaxBaitReachedException(Bait bait, ApplicationResult recoveryResult) {
+    public MaxBaitReachedException(@NotNull Bait bait, @NotNull ApplicationResult recoveryResult) {
         super(bait.getName() + " has reached its maximum number of uses on the fishing rod.");
         this.recoveryResult = recoveryResult;
     }
@@ -15,7 +16,8 @@ public class MaxBaitReachedException extends Exception {
     /**
      * @return The interrupted ApplicationResult object that would have been returned if it weren't for the error.
      */
-    public ApplicationResult getRecoveryResult() {
+    public @NotNull ApplicationResult getRecoveryResult() {
         return recoveryResult;
     }
+
 }
